f01bc10872a146baa94d8dff9b251ab64372dca3
Teerasej Pon <training@nextflow.in.th>
2020-10-17 13:52:08 +0700

    make it better

---
+++ src/config/mongoose.js
@@ -0,0 +1,24 @@
+'use strict'
+
+const mongoose = require('mongoose');
+const MONGODB_URI = process.env.MONGO_DB_URI || process.env.MONGODB_URI || "mongodb+srv://covid19_01:PP2537wkd@covid19.ivklh.mongodb.net/coivid19?retryWrites=true&w=majority"
+module.exports = function () {
+    mongoose.set('debug', process.env.MONGO_DB_URI || process.env.MONGODB_URI ? false : true);
+    let db = mongoose.connect(MONGODB_URI, function (err) {
+        if (err) {
+            console.log("MongoDB Notconnected..." + err);
+        } else {
+            console.log("MongoDB Connected...");
+        }
+
+    });
+
+    var glob = require('glob'),
+        path = require('path');
+
+    glob.sync(path.join(__dirname, '../modules/**/models/*.js')).forEach(function (file) {
+        require(path.resolve(file));
+    });
+
+    return db;
+}
